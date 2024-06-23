c1c4dba989d7b716891903ef90abd7ac0f00204d
shailesh yadav <shaileshyadv1993@gmail.com>
2024-06-23 22:01:53 +0545

    create signup page UI

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -14,6 +16,7 @@ mongoose
   });
 
 let PORT = 3000;
+
 const app = express();
 
 app.use(express.json());
