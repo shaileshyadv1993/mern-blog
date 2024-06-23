897de641123b701bb68eeff4ab304f8c257eb4cd
shailesh yadav <shaileshyadv1993@gmail.com>
2024-06-23 21:21:17 +0545

    add middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ mongoose
 
 let PORT = 3000;
 const app = express();
+
 app.use(express.json());
 
 app.listen(PORT, () => {
@@ -23,3 +24,8 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
